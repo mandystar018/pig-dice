5bdb62d59ebf087fe79f2be8b5c71e5b35ede387
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-11-04 15:02:21 -0800

    add information to user interface in js
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,46 @@
 function Game () {
   this.players = [];
-  this.currentId = 0;
-  this.score = 0;
+  // this.currentId = 0;
+  // this.score = 0;
 }
 
 Game.prototype.addPlayer = function(player) {
-  player.id = this.assignId();
+  // player.id = this.assignId();
   this.players.push(player);
 }
 
-Game.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
+function Player() {
+  this.currentScore = 0;
+  this.totalScore = 0;
 }
 
-function Player()
+let playerOne = new Player("Player One");
+let playerTwo = new Player("Player Two");
 
 
 
+Player.prototype.roll = function () {
+  let roll = Math.floor((Math.random() * 6 ) +1 ); 
+}
 
 
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
+
+
+// Game.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
 //loop that runs thru minium 7 with Math.random
+
+
+// User Interface Logic --------------------
+
+$(document).ready(function() {
+  const player1 = $("#player-one").val();
+  const player2 = $("#player-two").val();
+
+  
+}
